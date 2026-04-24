fc20f7e1575569457e6973f8204603f681f3ca39
Shruti <shrutimp09@gmail.com>
2026-04-24 11:39:12 +0530

    new changes for locations

---
+++ Humatrix_HRMS/wwwroot/js/mapPicker.js
@@ -0,0 +1,32 @@
+﻿window.initMapPicker = (dotnetRef) => {
+    const defaultLocation = { lat: 23.0225, lng: 72.5714 }; // Ahmedabad default
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: defaultLocation,
+        zoom: 13
+    });
+
+    let marker = new google.maps.Marker({
+        position: defaultLocation,
+        map: map,
+        draggable: true
+    });
+
+    // Click on map
+    map.addListener("click", (e) => {
+        const lat = e.latLng.lat();
+        const lng = e.latLng.lng();
+
+        marker.setPosition(e.latLng);
+
+        dotnetRef.invokeMethodAsync("SetLocation", lat, lng);
+    });
+
+    // Drag marker
+    marker.addListener("dragend", (e) => {
+        const lat = e.latLng.lat();
+        const lng = e.latLng.lng();
+
+        dotnetRef.invokeMethodAsync("SetLocation", lat, lng);
+    });
+};
