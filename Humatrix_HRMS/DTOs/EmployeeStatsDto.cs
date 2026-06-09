099153d757fa96edcd7658fcc6586e121407536c
Kashak1925 <patelkashak1925@gmail.com>
2026-06-09 17:40:59 +0530

    employee list

---
+++ Humatrix_HRMS/DTOs/EmployeeStatsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Humatrix_HRMS.DTOs
+{
+    public class EmployeeStatsDto
+    {
+        public int TotalWorkers { get; set; }
+        public int TotalEmployees { get; set; }
+        public int TotalHrs { get; set; }
+        public int TotalInactive { get; set; }
+    }
+}
